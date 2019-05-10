6fcb238ea376242be6ee5370b611595972237684
Reinaldo <masterrey@gmail.com>
2019-05-10 10:55:09 -0300

    Luz dinamica

---
+++ Assets/DayTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTime : MonoBehaviour
+{
+    public float daySeconds;
+    float radDay;
+    public float daySpeed = 1;
+    Light sunlight;
+    float intensity;
+    public Gradient ambient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sunlight = GetComponent<Light>();
+    }
+    //86400
+    // Update is called once per frame
+    void Update()
+    {
+        daySeconds += Time.deltaTime* daySpeed;
+        radDay = daySeconds / (86400/2);
+        transform.localRotation = Quaternion.Euler(radDay * (180 / Mathf.PI), 0, 0);
+
+        intensity = Mathf.Clamp01(Vector3.Dot(transform.forward, Vector3.down)+0.3f);
+        sunlight.intensity = intensity;
+
+        RenderSettings.ambientLight = ambient.Evaluate(intensity);
+
+        RenderSettings.fogDensity = 0.001f * intensity;
+    }
+}
