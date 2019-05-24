cb61cb734825b7e575ea0edb14a8e485164a1c51
masterrey@gmail.com <masterrey@gmail.com>
2019-05-24 10:50:18 -0300

    som dia noite arrumacao e animacao de soco

---
+++ Assets/Scripts/AmbienceChanger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceChanger : MonoBehaviour
+{
+  
+    public AudioSource sourceday;
+    public AudioSource sourcenight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DayTime.instance.DuskCall += morning;
+        DayTime.instance.DawnCall += afternoon;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void morning()
+    {
+        StartCoroutine("Ajusttoday");
+        
+    }
+
+    IEnumerator Ajusttoday()
+    {
+        while (sourceday.volume < 1)
+        {
+            sourceday.volume += DayTime.instance.daySpeed/(86400/4)*Time.deltaTime;
+            sourcenight.volume -= DayTime.instance.daySpeed/(86400/4) * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+          
+        }
+    }
+    IEnumerator Ajusttonight()
+    {
+        while (sourcenight.volume < 1)
+        {
+            sourceday.volume -= DayTime.instance.daySpeed / (86400 / 2) * Time.deltaTime;
+            sourcenight.volume += DayTime.instance.daySpeed / (86400 / 2) * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+           
+        }
+    }
+    void afternoon()
+    {
+        StartCoroutine("Ajusttonight");
+       
+    }
+}
---
+++ Assets/Scripts/CamChan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamChan : MonoBehaviour
+{
+    public GameObject player;
+    public float height, heightlook, distance, tolerance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform.position+Vector3.up*heightlook);
+        //posicao ideal
+        Vector3 dir =  Vector3.up * height + player.transform.forward * distance;
+
+        Vector3 postogo = player.transform.position +
+            Vector3.up * height +player.transform.forward * distance;
+        
+        RaycastHit hit;
+        if(Physics.Raycast(player.transform.position + Vector3.up * heightlook, dir, out hit,10))
+        {
+            postogo = hit.point- dir.normalized*tolerance;
+        }
+        
+        transform.position = Vector3.Lerp(transform.position, postogo, Time.smoothDeltaTime);
+    }
+}
