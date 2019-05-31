4a61a30a58d47b36ff4c02edfd0024f7ef478e55
Reinaldo <masterrey@gmail.com>
2019-05-31 08:53:35 -0300

    camera cinemachine

--- Assets/Scripts/MoveChan.cs
+++ Assets/Scripts/MoveChan.cs
@@ -7,22 +7,30 @@ public class MoveChan : MonoBehaviour
     public CharacterController charctrl;
     public Animator anim;
     Vector3 movaxis, turnaxis;
+    public GameObject currentCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentCamera = Camera.main.gameObject;
     }
 
     void FixedUpdate()
     {
 
-        movaxis = new Vector3(0, 0, Input.GetAxis("Vertical"));
-        turnaxis = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        movaxis = new Vector3(Input.GetAxis("Horizontal")*0.3f, 0, Input.GetAxis("Vertical"));
+       
 
         charctrl.SimpleMove(transform.TransformVector(movaxis)*3);
 
         anim.SetFloat("Speed", charctrl.velocity.magnitude);
-        transform.Rotate(turnaxis);
+        
+
+        Vector3 dirtogo = new Vector3(currentCamera.transform.forward.x, 0
+            , currentCamera.transform.forward.z);
+
+        Quaternion rottogo = Quaternion.LookRotation( (movaxis.magnitude*dirtogo*2 + transform.forward));
+
+        transform.rotation = Quaternion.Lerp(transform.rotation,rottogo,Time.fixedDeltaTime*3);
 
         if (Input.GetButtonDown("Fire1"))
         {
