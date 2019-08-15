70dadb58c346f031854735ec847a0c0d9e6caac5
MasterRey <masterrey@gmail.com>
2019-08-15 11:16:39 -0300

    Agua , e shrines  inicial

---
+++ Assets/Scripts/Shrine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shrine : MonoBehaviour
+{
+    public string ShrineName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(ShrineName);
+        }
+    }
+}
